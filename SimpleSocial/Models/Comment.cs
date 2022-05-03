5eab73284ac49f0ed679000aefdcb931b738aea3
Goronmon <dev@tsgagnon.com>
2022-05-02 21:21:47 -0400

    Expand on Comment and Post models

--- SimpleSocial/Models/Comment.cs
+++ SimpleSocial/Models/Comment.cs
@@ -6,10 +6,17 @@ namespace SimpleSocial.Models
     public class Comment
     {
         public int CommentId { get; set; }
+        [Required]
         public string Text { get; set;}
+        public bool IsDeleted { get; set; } = false;
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public DateTime CreateDate { get; set; }
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public DateTime LastModifiedDate { get; set; }
 
+        [Required]
         [ForeignKey("User")]
         public string UserId { get; set; }
         public User User { get; set; }
@@ -18,20 +25,31 @@ namespace SimpleSocial.Models
         public int PostId { get; set; }
         public Post Post { get; set; }
 
+        
+        [ForeignKey("Comment")]
+        public int ParentCommentId { get; set; }
+        public Comment ParentComment { get; set; }
+
+        public List<Comment> Comments { get; set; }
     }
 
     public class CommentDelta
     {
         public int PostDeltaId { get;set; }
+        [Required]
         public int Sequence { get; set; }
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public DateTime UpdateDate { get; set; }
         public string OldText { get; set; }
         public string NewText { get; set; }
 
+        [Required]
         [ForeignKey("User")]
         public string UserId { get; set; }
         public User User { get; set; }
 
+        [Required]
         [ForeignKey("Post")]
         public int PostId { get; set; }
         public Post Post { get; set; }
@@ -40,15 +58,20 @@ namespace SimpleSocial.Models
     public class CommentReport
     {
         public int CommentReportId { get; set; }
+        [Required]
         public string Reason { get; set; }
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public DateTime ReportDate { get; set; }
 
+        [Required]
         [ForeignKey("User")]
         public string UserId { get; set; }
         public User User { get; set; }
 
-        [ForeignKey("Post")]
-        public int PostId { get; set; }
-        public Post Post { get; set; }
+        [Required]
+        [ForeignKey("Comment")]
+        public int CommentId { get; set; }
+        public Comment Comment { get; set; }
     }
 }
--- SimpleSocial/Models/Post.cs
+++ SimpleSocial/Models/Post.cs
@@ -6,32 +6,45 @@ namespace SimpleSocial.Models
     public class Post
     {
         public int PostId { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Url { get; set; }
         public string Text { get; set;}
+        public bool IsDeleted { get; set; } = false;
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public DateTime CreateDate { get; set; }
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public DateTime LastModifiedDate { get; set; }
 
+        [Required]
         [ForeignKey("User")]
         public string UserId { get; set; }
         public User User { get; set; }
 
+        public List<Comment> Comments { get; set; }
     }
 
     public class PostDelta
     {
         public int PostDeltaId { get;set; }
+        [Required]
         public int Sequence { get; set; }
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public DateTime UpdateDate { get; set; }
         public string OldTitle { get; set; }
         public string NewTitle { get; set; }
         public string OldText { get; set; }
         public string NewText { get; set; }
 
+        [Required]
         [ForeignKey("User")]
         public string UserId { get; set; }
         public User User { get; set; }
 
+        [Required]
         [ForeignKey("Post")]
         public int PostId { get; set; }
         public Post Post { get; set; }
@@ -40,13 +53,18 @@ namespace SimpleSocial.Models
     public class PostReport
     {
         public int PostReportId { get; set; }
+        [Required]
         public string Reason { get; set; }
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public DateTime ReportDate { get; set; }
 
+        [Required]
         [ForeignKey("User")]
         public string UserId { get; set; }
         public User User { get; set; }
 
+        [Required]
         [ForeignKey("Post")]
         public int PostId { get; set; }
         public Post Post { get; set; }
